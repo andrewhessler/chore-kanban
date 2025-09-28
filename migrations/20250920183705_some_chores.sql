de7c1bb7272ebf968b28da8cde8839bbf9c4525c
Andrew Hessler <adhessler@gmail.com>
2025-09-27 19:51:10 -0500

    unmodify?

--- migrations/20250920183705_some_chores.sql
+++ migrations/20250920183705_some_chores.sql
@@ -1,2 +1,2 @@
-INSERT INTO chores (display_name, frequency_hours, last_completed_at)
-VALUES ("Laundry", NULL, 1758574800);
+INSERT INTO chores (display_name, frequency_hours)
+VALUES ("Laundry", NULL)
