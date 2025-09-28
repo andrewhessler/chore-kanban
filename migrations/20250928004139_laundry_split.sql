f2639fbb72705972385b6a62cfc0771511287c9b
Andrew Hessler <adhessler@gmail.com>
2025-09-27 19:46:07 -0500

    add some chores

---
+++ migrations/20250928004139_laundry_split.sql
@@ -0,0 +1,4 @@
+UPDATE chores SET display_name = "Laundry Wash" WHERE display_name = "Laundry";
+INSERT INTO chores (display_name, frequency_hours, on_cadence, last_completed_at)
+VALUES 
+("Laundry Put Away", NULL, 0, 1758574800); -- Every 2 weeks, Monday
